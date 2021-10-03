7bb7adf2a566575eec30898a8c722cb69c0a61dd
Aayushi2010 <80824357+Aayushi2010@users.noreply.github.com>
2021-10-03 17:37:23 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function new_image()
+
+{
+    fabric.Image.fromURL('HAPPYBDAY.jpeg'), function (Image) {
+        block_image_object = Img;
+        
+        block_img_object.scaleToWidth(700);
+        block_img_object.scaleToHeight(510);
+        block_img_object.set({
+            top: 0,
+            left: 0
+    });
+
+    canvas.add(block_image_object)
+});
+}
